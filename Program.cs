a097bb1130fe89e95428a608d815b948f19f860d
NikitaKhasdeo <101544635+NikitaKhasdeo@users.noreply.github.com>
2022-03-15 00:31:32 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyFirstCSharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.PrintData(100);
+            System.Console.Write(1200);
+        }
+    }
+
+    // Create your own Console class
+    class Console 
+    {
+        // Method to print data to Console window
+        public static void PrintData(int n)
+        {
+            System.Console.WriteLine(n);
+        }
+
+        public static void WriteLine(string s)
+        {
+            System.Console.WriteLine(s);
+        }
+    }
+
+
+
+}
